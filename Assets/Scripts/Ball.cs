3e5f150211ead4027722afecc41590628acfddba
Kemal <45937663+BesBlet@users.noreply.github.com>
2020-10-29 20:54:00 +0300

    ver. 0.8

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public Pad pad;
+    bool isStarted;
+    float yPosition;
+
+
+    void Start()
+    {
+        yPosition = transform.position.y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision!");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        print("Collision Exit!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger!");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("Trigger exit!");
+    }
+
+
+
+    void Update()
+    {
+        if(isStarted)
+        {
+
+        }
+
+        else
+
+        {
+            Vector3 padPosition = pad.transform.position;
+            Vector3 BallNewPosition = new Vector3(padPosition.x, padPosition.y + yPosition, 0);
+            transform.position = BallNewPosition;
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Start!");
+                StartBall();
+            }
+        }
+
+    }
+
+   void StartBall()
+    {
+        isStarted = true;
+        Vector2 force = new Vector2(10, 1) * speed;
+        rb.AddForce(force);
+    }
+}
+
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    float yPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePixelPosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePixelPosition);
+
+        Vector3 padNewPosition = mouseWorldPosition;
+
+
+        padNewPosition.z = 0;
+
+        padNewPosition.y = yPosition;
+
+
+
+        transform.position = padNewPosition;
+    }
+}
